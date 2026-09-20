09ad9b2b05264cba5bc64dc9f7d6625b7f907697
Spezialeinheit Brandenburg <allianzgruppe.darkbulls@gmail.com>
2026-09-20 21:51:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Zentrale Konfiguration für das BTH PLANWERK
 window.BTH_CONFIG = {
   // Google Apps Script WebApp-URL als API
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbytjpTP1X9Aq2DLNfJM2QZe3c1oxqW9vmdsFouchDoMmG6pYKwdT1shrV7FuVck4IMz/exec"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz7ESGS5Ep_-H_Yoy3rIzVwqXnamkEWSf8_aD841VBxAPckh9bJUZ5ow9wdgUF5bR858A/exec"
 };
