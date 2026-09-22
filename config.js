7ef5e7c1a00df977d263ca4e66867b3b92ac62a4
Spezialeinheit Brandenburg <allianzgruppe.darkbulls@gmail.com>
2026-09-22 07:57:48 +0200

    Update WEB_APP_URL to webAppUrl in config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.BTH_CONFIG = {
-    // Google Apps Script WebApp-URL als API
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxnDQlxzJ8pas9khajjSamNXjJEHMlWI2YjZQ_Z-OTjyc1gUpUdNMaaoMaluBWUQmMCRw/exec"
+    webAppUrl: "https://script.google.com/macros/s/AKfycbxnDQlxzJ8pas9khajjSamNXjJEHMlWI2YjZQ_Z-OTjyc1gUpUdNMaaoMaluBWUQmMCRw/exec"
 };
