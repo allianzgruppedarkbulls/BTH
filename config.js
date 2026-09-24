894df47e5e3f773f757ddab152889544ef8c51aa
Spezialeinheit Brandenburg <allianzgruppe.darkbulls@gmail.com>
2026-09-25 01:51:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 window.BTH_CONFIG = {
-    webUrl:     "https://script.google.com/macros/s/AKfycbxKqeywH3IUxTzIPCjJYCyHKCxScPXATS2rLhLjR34t-GwFHA1gquc",
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxKqeywH3IUxTzIPCjJYCyHKCxScPXATS2rLhLjR34t-GwFHA1gquc",
-    webAppUrl: "https://script.google.com/macros/s/AKfycbxKqeywH3IUxTzIUxTzIPCjJYCyHKCxScPXATS2rLhLjR34t-GwFHA1gquc"
+    webUrl:     "https://script.google.com/macros/s/AKfycbxKqeywH3IUxTzIPCjYCyHKcXscPXATS2rLhLjr34t-GwFHA1gquokSMzmsF8mR7TxHwA/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxKqeywH3IUxTzIPCjYCyHKcXscPXATS2rLhLjr34t-GwFHA1gquokSMzmsF8mR7TxHwA/exec",
+    webAppUrl: "https://script.google.com/macros/s/AKfycbxKqeywH3IUxTzIPCjYCyHKcXscPXATS2rLhLjr34t-GwFHA1gquokSMzmsF8mR7TxHwA/exec"
 };
 ```[cite: 3]
 
