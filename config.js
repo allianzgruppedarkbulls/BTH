9043d405576b2e706fe0b7ea8e72002047064cbd
Spezialeinheit Brandenburg <allianzgruppe.darkbulls@gmail.com>
2026-09-24 18:21:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
+
 window.BTH_CONFIG = {
-    webAppUrl: "https://script.google.com/macros/s/AKfycbxKqeywH3IUxTzIPCjYCyHKcXscPXATS2rLhLjr34t-GwFHA1gquokSMzmsF8mR7TxHwA/exec"
+    webUrl:     "https://script.google.com/macros/s/AKfycbxKqeywH3IUxTzIPCjJYCyHKCxScPXATS2rLhLjR34t-GwFHA1gquc",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxKqeywH3IUxTzIPCjJYCyHKCxScPXATS2rLhLjR34t-GwFHA1gquc",
+    webAppUrl: "https://script.google.com/macros/s/AKfycbxKqeywH3IUxTzIUxTzIPCjJYCyHKCxScPXATS2rLhLjR34t-GwFHA1gquc"
 };
+```[cite: 3]
+
+Wenn du das einmal kurz in deiner `config.js` hinterlegst, kannst du in den einzelnen HTML-Dateien völlig frei sein – der Login und die anderen Seiten werden sofort wieder fehlerfrei anbinden!
