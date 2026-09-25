492873ec6479003b1cc74d6135ad79f3e42ec0d7
Spezialeinheit Brandenburg <allianzgruppe.darkbulls@gmail.com>
2026-09-25 02:51:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,28 +1,4 @@
-const CONFIG = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx4U8UZgrbBtYPuTLXH8O21MeRYLj4GgLYQjceQdWKJC1awxR4TBypm28Clfl8Q2n-kJQ/exec",
-    APP_NAME: "BTH PLANWERK",
-    VERSION: "2.5.0",
-    
-    async apiCall(action, payload = {}) {
-        try {
-            const response = await fetch(this.SCRIPT_URL, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "text/plain;charset=utf-8", 
-                },
-                body: JSON.stringify({ action, ...payload })
-            });
-            const result = await response.json();
-            return result;
-        } catch (error) {
-            console.error("API-Fehler bei Aktion:", action, error);
-            return { success: false, error: error.message };
-        }
-    }
-};
 
-// Kompatibilitätsschicht für Skripte, die BTH_CONFIG erwarten:
-const BTH_CONFIG = {
-    webAppUrl: CONFIG.SCRIPT_URL,
-    scriptUrl: CONFIG.SCRIPT_URL
+const CONFIG = {
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx4U8UZgrbBtYPuTLXH8O21MeRYLj4GgLYQjceQdWKJC1awxR4TBypm28Clfl8Q2n-kJQ/exec"
 };
